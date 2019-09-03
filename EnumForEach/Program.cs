d286e35a71f1e050a67ea166104d43da0a01abbe
AditiRupade <arupade@tavisca.com>
2019-09-03 14:57:29 +0530

    first commit

---
+++ EnumForEach/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnumForEach
+{
+    public class Person
+    {
+        public Person(string fName, string lName)
+        {
+            this.firstName = fName;
+            this.lastName = lName;
+        }
+
+        public string firstName;
+        public string lastName;
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] peopleArray = new Person[3]
+        {
+            new Person("John", "Smith"),
+            new Person("Jim", "Johnson"),
+            new Person("Sue", "Rabon"),
+        };
+            List<Person> x = new List<Person>();
+            x.Add(new Person("John", "Smith"));
+            x.Add(new Person("Jim", "Smith"));
+            x.Add(new Person("Sue", "Smith"));
+
+            foreach (Person p in x)
+                Console.WriteLine(p.firstName + " " + p.lastName);
+        }
+    }
+}
---
+++ EnumerableDemo/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+
+public class Person
+{
+    public Person(string fName, string lName)
+    {
+        this.firstName = fName;
+        this.lastName = lName;
+    }
+
+    public string firstName;
+    public string lastName;
+}
+
+public class People : IEnumerable
+{
+    private Person[] _people;
+    public People(Person[] pArray)
+    {
+        _people = pArray;
+      
+    }
+
+    //methods
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return new PeopleEnum(_people);
+    }
+
+}
+
+public class PeopleEnum : IEnumerator
+{
+    public Person[] _people;
+
+    int position = -1;
+
+    public PeopleEnum(Person[] list)
+    {
+        _people = list;
+    }
+
+    public bool MoveNext()
+    {
+        position++;
+        Console.WriteLine("here");
+        return (position < _people.Length);
+    }
+
+    public void Reset()
+    {
+        position = -1;
+    }
+
+    object IEnumerator.Current
+    {
+        get
+        {
+                return _people[position];
+           
+        }
+    }
+
+}
+
+
+class App
+{
+    static void Main()
+    {
+
+        //explicitly coded
+
+        Person[] peopleArray = new Person[3]
+        {
+            new Person("John", "Smith"),
+            new Person("Jim", "Johnson"),
+            new Person("Sue", "Rabon"),
+        };
+
+        People peopleList = new People(peopleArray);
+        foreach (Person p in peopleList)
+            Console.WriteLine(p.firstName + " " + p.lastName);
+
+    }
+}
+
